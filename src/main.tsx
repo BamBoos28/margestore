f86448f758230af16f0f90b6bf872165e8450dc9
BamBoos28 <119471667+BamBoos28@users.noreply.github.com>
2026-01-09 19:33:12 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import ReactDOM from "react-dom/client";
 import { ToastProvider } from "./components/ToastProvider";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./App.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <HashRouter>
+  <BrowserRouter>
     <ToastProvider>
       <App />
     </ToastProvider>
-  </HashRouter>
+  </BrowserRouter>,
 );
